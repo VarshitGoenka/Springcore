5dd60d0903ce6e2f86497c7779396a8b211ec094
varshitgoenka@gmail.com <varshitgoenka@gmail.com>
2022-05-18 18:04:02 +0530

    Day-1 Spring core concepts

---
+++ src/main/java/com/ust1/spring/springcore/map/Customer.java
@@ -0,0 +1,32 @@
+package com.ust1.spring.springcore.map;
+
+import java.util.Map;
+
+public class Customer {
+
+	private String name;
+	private Map<Integer, String> products;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Map<Integer, String> getProducts() {
+		return products;
+	}
+
+	public void setProducts(Map<Integer, String> products) {
+		this.products = products;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", products=" + products + "]";
+	}
+
+}
